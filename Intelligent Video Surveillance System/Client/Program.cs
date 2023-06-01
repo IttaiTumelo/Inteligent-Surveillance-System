7f121821c599e010a2b89b92d61ea68c8fcd19f6
Ittai Tumelo <ittaitumelo@outlook.com>
2023-06-01 22:41:49 +0200

    Initial

---
+++ Intelligent Video Surveillance System/Client/Program.cs
@@ -0,0 +1,11 @@
+using Intelligent_Video_Surveillance_System.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
