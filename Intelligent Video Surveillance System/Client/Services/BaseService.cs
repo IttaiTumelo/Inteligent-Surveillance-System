0be5b068b3162a408779743df4e795142a437c65
Ittai Tumelo <ittaitumelo@outlook.com>
2023-06-03 10:37:31 +0200

    update trial

---
+++ Intelligent Video Surveillance System/Client/Services/BaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace Intelligent_Video_Surveillance_System.Client.Services
+{
+    public class BaseService
+    {
+        protected readonly HttpClient _httpClient;
+        public BaseService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
